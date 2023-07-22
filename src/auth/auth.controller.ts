864c76bec58e74a409bef1300b09f25c96de951b
Jose Alejandro Rojano Crisson <j053noir@gmail.com>
2023-07-21 22:04:30 -0500

    Create sign in endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './DTOs/auth-crendential.dto';
+import { JwtResponse } from './jwt-response.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,12 @@ export class AuthController {
   @Post('signup')
   public signUp(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.CreateUser(authCredentialsDto);
+  }
+
+  @Post('signin')
+  public signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<JwtResponse> {
+    return this.authService.VerifyUser(authCredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,23 @@
 import { Inject, Injectable } from '@nestjs/common';
 import {
+  BadRequestException,
   ConflictException,
   InternalServerErrorException,
 } from '@nestjs/common/exceptions';
+import { UnauthorizedException } from '@nestjs/common/exceptions/unauthorized.exception';
+import { JwtService } from '@nestjs/jwt/dist';
+import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './DTOs/auth-crendential.dto';
+import { JwtPayload } from './jwt-payload.interface';
+import { JwtResponse } from './jwt-response.interface';
 import { User } from './user.entity';
-import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @Inject('USER_REPOSITORY') private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   public async CreateUser(createUserDto: AuthCredentialsDto): Promise<void> {
@@ -33,5 +39,25 @@ export class AuthService {
         throw new ConflictException('Username is not available');
       else throw new InternalServerErrorException();
     }
+  }
+
+  public async VerifyUser(
+    autheCredentialsDto: AuthCredentialsDto,
+  ): Promise<JwtResponse> {
+    const { username, password } = autheCredentialsDto;
+
+    if (!username || !password)
+      throw new BadRequestException('Please provide a username and password');
+
+    const user = await this.userRepository.findOneBy({ username });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Please validate your credentials');
+    }
+
+    const payload: JwtPayload = { username };
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken };
   }
 }
